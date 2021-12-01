5c561bbc42eef27ca196032ea722ee4ecbe0e702
German Arevalo J <germanarevalojerez@gmail.com>
2021-12-01 08:41:35 -0500

    init repository

---
+++ src/app/guards/intro.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { Storage } from '@ionic/storage';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IntroGuard implements CanActivate {
+  constructor(private storage: Storage, private router: Router) {}
+
+  async canActivate() {
+    const isIntroShowed = await this.storage.get('isIntroShowed');
+    if (isIntroShowed) {
+      return true;
+    } else {
+      this.router.navigateByUrl('/intro');
+    }
+  }
+}
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
+        ],
+      ],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit() {}
+
+  loginUser() {}
+}
